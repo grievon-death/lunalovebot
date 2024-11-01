f91f12139e23dd8338194f22d1a6a466ef543ad4
Kevin Caires <kevin.caires@smartgreen.net>
2024-11-01 07:00:58 -0300

    [ADD] - Comandos iniciais.

--- core/bot.py
+++ core/bot.py
@@ -1,13 +1,17 @@
 import logging
+from random import choice
+from datetime import datetime
 
 from discord import Embed, Intents, Message
 from discord.ext.commands import Context, Bot
 
-
+from core import naive_dt_utc_br
 from models.quote import Quotes
 from models.indicator import Indicators
 
 LOGGER = logging.getLogger(__name__)
+ERROR_MESSAGE = '**Ooops.**\n> Alguma coisa deu errado, melhor fuçar os logs!'
+ARG_FAULT = '**Ooops.**\n> É necessária uma mensagem!'
 
 client = Bot(command_prefix='--', intents=Intents.all())
 
@@ -19,7 +23,7 @@ async def on_message(message: Message) -> None:
 
 
 @client.command(aliases=['q'])
-async def quote(ctx: Context) -> str:
+async def quote(ctx: Context) -> None:
     """
     Salva uma mensagem no banco de dados.
     """
@@ -30,17 +34,105 @@ async def quote(ctx: Context) -> str:
     message = ' '.join(message)
 
     if not message:
-        return await ctx.send('Ooops, você não escreveu nada!')
+        await ctx.send(ARG_FAULT)
+        return
 
     try:
-        model = Quotes()
-        model.message = message
-        model.created_by = ctx.author.name
-        model.server = server
-        response = await model.create()
+        model = Quotes(
+            message=message,
+            server=server,
+            created_by=ctx.author.name,
+            created_at=datetime.now(),
+        )
+        model.create()
+        indicator = Indicators(server)
+        await indicator.add_quote_for(ctx.author.name)
     except Exception as e:
         LOGGER.error(e)
-        return await ctx.send(f'Foi mal, aconteceu que {str(e.args)}')
+        await ctx.send(ERROR_MESSAGE)
+        return
 
-    # TODO: Fazer embed no response.
-    return await ctx.send(str(response))
+    try:
+        embed = Embed(type='rich', )
+        embed.add_field(
+            name='Usuário',
+            value=model.created_by,
+        )
+        embed.add_field(
+            name='Data',
+            value=naive_dt_utc_br(model.created_at),
+        )
+        embed.add_field(
+            name='Mensagem',
+            value=model.message,
+            inline=False,
+        )
+        await ctx.send(embed=embed)
+    except Exception as e:
+        LOGGER.exception(e)
+        await ctx.send(ERROR_MESSAGE)
+
+
+@client.command(aliases=['rq'])
+async def randomquote(ctx: Context) -> None:
+    """
+    Captura uma mensasgem aleatória.
+    """
+    try:
+        server = ctx.guild.id
+        model = Quotes()
+        ids = model.get_ids_by_server(server)
+        id = choice(ids)
+        quote = model.get(id)
+
+        if not quote:
+            await ctx.send(ERROR_MESSAGE)
+
+        indicator = Indicators(server)
+        await indicator.add_request_for(ctx.author.name)
+        await ctx.send(quote.message)
+    except Exception as e:
+        LOGGER.error(e)
+        await ctx.send(ERROR_MESSAGE)
+
+
+@client.command(aliases=['i'])
+async def indicators(ctx: Context, quantity: int=5) -> None:
+    """
+    Retorna o TOP criadores de mensagens e os que mais usam o comando randomquote.
+    quantity: int :Tamanho da lista.
+    """
+    server = ctx.guild.id
+    indicator = Indicators(server=server)
+
+    try:
+        quoters = await indicator.get_quoters()
+        requesters = await indicator.get_requesters()
+        quoters_k = sorted(quoters, key=quoters.get, reverse=True) if quoters else []
+        requesters_k = sorted(requesters, key=requesters.get, reverse=True) if requesters else []
+    except Exception as e:
+        LOGGER.error(e)
+        await ctx.send(ERROR_MESSAGE)
+        return
+
+    try:
+        _quoters = Embed(type='rich')
+        _requesters = Embed(type='rich')
+
+        for key in quoters_k[quantity:]:
+            _quoters.add_field(
+                name=key,
+                value=quoters[key],
+            )
+
+        for key in requesters_k[quantity:]:
+            _requesters.add_field(
+                name=key,
+                value=requesters[key],
+            )
+
+        await ctx.send(f'**Top {quantity} criadores:**', embed=_quoters) if quoters else None
+        await ctx.send(f'**Top {quantity}:**', embed=_requesters) if requesters else None
+    except Exception as e:
+        LOGGER.error(e)
+        await ctx.send(ERROR_MESSAGE)
