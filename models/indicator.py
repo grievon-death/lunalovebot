d45846e67fee63e8d2063e91d7a8a1358aa36c0c
Kevin Caires <kevin.caires@smartgreen.net>
2024-11-01 07:00:05 -0300

    [FIX] - Arruma os indicadores e a migração da collection.

--- models/indicator.py
+++ models/indicator.py
@@ -21,110 +21,121 @@ class Indicators:
         """
         Cria as condições de banco para os indicadores.
         """
-        async def migrate():
-            db = mongo_client.get_database(settings.MONGODB_NAME)
-            quoters = db.get_collection('quoters')
-            requesters = db.get_collection('requesters')
+        try:
+            async def migrate():
+                db = mongo_client.get_database(settings.MONGODB_NAME)
+                quoters = db.get_collection('quoters')
+                requesters = db.get_collection('requesters')
 
-            try:
-                await quoters.create_index('server', unique=True)
-            except Exception as e:
-                LOGGER.debug(e)
+                try:
+                    await quoters.create_index('server', unique=True)
+                except Exception as e:
+                    LOGGER.debug(e)
 
-            try:
-                await requesters.create_index('server', unique=True)
-            except Exception as e:
-                LOGGER.debug(e)
+                try:
+                    await requesters.create_index('server', unique=True)
+                except Exception as e:
+                    LOGGER.debug(e)
 
-        asyncio.run(migrate())
+            asyncio.run(migrate())
+        except Exception as e:
+            raise e
 
-    async def calculate_quoters(self) -> None:
+    async def add_quote_for(self, username: str) -> None:
         """
-        Cria o indicador para determinado servidor.
+        Adiciona no contador de quotes.
         """
-        LOGGER.info('Init indicator quoters process for server %s.', self.server)
-        coll = self.db.get_collection(f'quoters')
-        _exists = coll.find_one({ 'server': self.server })
-        _document = dict()
+        try:
+            LOGGER.info('Add one more quote to %s', username)
+            coll = self.db.get_collection('quoters')
+            quoters = await coll.find_one({ 'server': self.server })
+            _documents = dict(quoters) if quoters else None
 
-        async with sql_engine.begin() as session:
-            stmt = text(f"""
-                select created_by, count(id)
-                from quotes
-                where server = {self.server}
-                group by created_by;
-            """)
-            cursor = await session.execute(stmt)
-            response = cursor.scalars().all()
+            if not _documents:
+                coll.insert_one({
+                    'server': self.server,
+                    username: 1
+                })
+                return
 
-        for name, total in response:
-            _document[name] = total
+            _documents.pop('server')
 
-        if not _exists:
-            _document['server'] = self.server
-            coll.insert_one(_document)
-        else:
-            coll.update_one(
-                { 'server': self.server },
-                _document,
+            if not _documents.get(username):
+                _documents[username] = 1
+            else:
+                _documents[username] += 1
+
+            await coll.update_one(
+                {'server': self.server},
+                _documents,
             )
-
-        LOGGER.info('Finish indicator quoters process for server %s.', self.server)
+        except Exception as e:
+            raise e
 
     async def add_request_for(self, username: str) -> None:
         """
-        Adiciona no contador de 
+        Adiciona no contador de requests.
         """
-        LOGGER.info('Add one more request for quote to %s', username)
-        coll = self.db.get_collection('requesters')
-        requesters = await coll.find_one({ 'server': self.server })
-        _documents = dict(requesters) if requesters else None
+        try:
+            LOGGER.info('Add one more request for quote to %s', username)
+            coll = self.db.get_collection('requesters')
+            requesters = await coll.find_one({ 'server': self.server })
+            _documents = dict(requesters) if requesters else None
 
-        if not _documents:  # Servidor sem indicador.
-            coll.insert_one({
-                'server': self.server,
-                username: 1,
-            })
-            return
+            if not _documents:  # Servidor sem indicador.
+                coll.insert_one({
+                    'server': self.server,
+                    username: 1,
+                })
+                return
 
-        _documents.pop('server')  # Campo que não sofre update.
+            _documents.pop('server')  # Campo que não sofre update.
 
-        if not _documents.get(username):  # Usuário sem indicador.
-            _documents[username] = 1
-        else:
-            _documents[username] += 1
+            if not _documents.get(username):  # Usuário sem indicador.
+                _documents[username] = 1
+            else:
+                _documents[username] += 1
 
-        await coll.update_one(
-            { 'server': self.server },
-            _documents,
-        )
+            await coll.update_one(
+                { 'server': self.server },
+                _documents,
+            )
+        except Exception as e:
+            raise e
 
     async def get_quoters(self) -> Optional[Dict]:
         """
         Captura o indicador de quotadores do server.
         """
-        LOGGER.info('Getting quoters for server %s', self.server)
-        coll = self.db.get_collection('quoters')
-        requesters = await coll.find_one({ 'server': self.server })
+        try:
+            LOGGER.info('Getting quoters for server %s', self.server)
+            coll = self.db.get_collection('quoters')
+            requesters = await coll.find_one({ 'server': self.server })
 
-        if not requesters:
-            return
+            if not requesters:
+                return
 
-        _response = dict(requesters)
-        _response.pop('server')
-        return _response
+            _response = dict(requesters)
+            _response.pop('server')
+            return _response
+        except Exception as e:
+            LOGGER.error(e)
+            raise e
 
     async def get_requesters(self) -> Optional[Dict]:
         """
         Captura o indicador de requisitores do server.
         """
-        LOGGER.info('Getting requesters for server %s', self.server)
-        coll = self.db.get_collection('requesters')
-        requesters = await coll.find_one({ 'server': self.server })
+        try:
+            LOGGER.info('Getting requesters for server %s', self.server)
+            coll = self.db.get_collection('requesters')
+            requesters = await coll.find_one({ 'server': self.server })
 
-        if not requesters:
-            return
+            if not requesters:
+                return
 
-        _response = dict(requesters)
-        _response.pop('server')
-        return _response
+            _response = dict(requesters)
+            _response.pop('server')
+            return _response
+        except Exception as e:
+            raise e
